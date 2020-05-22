868b0f4635706e205e25877d56805eb01b9d0c5a
via8 <viaborne8@gmail.com>
2020-05-22 18:02:30 +0300

    Update test.py for whole set of experiments data

--- CourseProject/test.py
+++ CourseProject/test.py
@@ -1,11 +1,13 @@
-import sys
 import os
+import sys
+import json
 from matplotlib import pyplot
 
-SHT_NUMBER = 38515
 SIGNAL_NUMBERS = [18, 19, 20, 26]
-SENSOR_NAMES = ['SXR 15 $\mu$m', 'SXR 27 $\mu$m', 'SXR 50 $\mu$m', 'SXR 80 $\mu$m']
-PATH_TO_SHT_FILE = "../SHT/sht" + str(SHT_NUMBER) + ".sht"
+SENSOR_NAMES = ['signal = 18, SXR 15 $\mu$m',
+                'signal = 19, SXR 27 $\mu$m',
+                'signal = 20, SXR 50 $\mu$m',
+                'signal = 26, SXR 80 $\mu$m']
 
 WORK_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.dirname(WORK_DIR)
@@ -21,24 +23,34 @@ except ImportError as error:
     sys.exit(-1)
 
 # Read using pyglobus
+data_file = open('data.json')
+data = json.load(data_file)
+for sht_data in data['sht_data']:
+    sht_number = sht_data[0]
+    path_to_sht_file = "../SHT/sht" + str(sht_number) + ".sht"
 
-sht_reader = pyglobus.util.ShtReader(PATH_TO_SHT_FILE)
-fig, ax = pyplot.subplots(nrows=2, ncols=2)
-for ind in range(0, len(SIGNAL_NUMBERS)):
-    signal_name = sht_reader.get_signal_name(SIGNAL_NUMBERS[ind])
-    signal = sht_reader.get_signals(signal_name)[0]
+    if not os.path.isfile(path_to_sht_file):
+        print('WARNING: unable process file\n' +
+              path_to_sht_file)
+        continue
 
-    x, y = signal.get_data_x(), signal.get_data_y()
+    sht_reader = pyglobus.util.ShtReader(path_to_sht_file)
+    fig, ax = pyplot.subplots(nrows=2, ncols=2)
+    for ind in range(0, len(SIGNAL_NUMBERS)):
+        signal_name = sht_reader.get_signal_name(SIGNAL_NUMBERS[ind])
+        signal = sht_reader.get_signals(signal_name)[0]
 
-    pyplot.subplot(2, 2, ind + 1)
+        x, y = signal.get_data_x(), signal.get_data_y()
 
-    pyplot.plot(x, y)
-    pyplot.xlim(0.15, 0.20)
-    pyplot.xlabel('Time, s')
-    pyplot.ylabel('V')
-    pyplot.title(SENSOR_NAMES[ind])
+        pyplot.subplot(2, 2, ind + 1)
 
-pyplot.tight_layout()
-if not os.path.exists(OUT_DIR):
-    os.makedirs(OUT_DIR)
-fig.savefig(OUT_DIR + 'sht' + str(SHT_NUMBER) + '.png')
+        pyplot.plot(x, y)
+        pyplot.xlim(0.10, 0.25)
+        pyplot.xlabel('Time, s')
+        pyplot.ylabel('V')
+        pyplot.title(SENSOR_NAMES[ind])
+
+    pyplot.tight_layout()
+    if not os.path.exists(OUT_DIR):
+        os.makedirs(OUT_DIR)
+    fig.savefig(OUT_DIR + 'sht' + str(sht_number) + '.png')
