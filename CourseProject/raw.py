23da93f9d07970c201824eb403275a1f62c24789
zlehk <zlehk@yahoo.com>
2020-06-12 23:34:30 +0300

    modify for cli

--- CourseProject/raw.py
+++ CourseProject/raw.py
@@ -1,24 +1,16 @@
 import os
 import sys
-import json
 from matplotlib import pyplot
+from CourseProject.signal_info import SIGNAL_NAMES
 
-SIGNAL_NUMBERS = [18, 19, 20, 26]
-SENSOR_NAMES = ['signal = 18, SXR 15 $\mu$m',
-                'signal = 19, SXR 27 $\mu$m',
-                'signal = 20, SXR 50 $\mu$m',
-                'signal = 26, SXR 80 $\mu$m']
-SIGNAL_NAMES = {
-    18: b'SXR 15 \xec\xea\xec\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
-    19: b'SXR 27 \xec\xea\xec\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
-    20: b'SXR 50 mkm\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
-    26: b'SXR 80 mkm\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-}
+SENSOR_NAMES = {18: 'signal = 18, SXR 15 ${\mu}m$',
+                19: 'signal = 19, SXR 27 ${\mu}m$',
+                20: 'signal = 20, SXR 50 ${\mu}m$',
+                26: 'signal = 26, SXR 80 ${\mu}m$'}
 
 WORK_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.dirname(WORK_DIR)
 PYGLOBUS_DIR = PARENT_DIR + '\\pyglobus\\python\\'
-OUT_DIR = WORK_DIR + '\\raw\\'
 
 sys.path.append(PYGLOBUS_DIR)
 
@@ -28,36 +20,26 @@ except ImportError as error:
     print('Unable to import pyglobus from ' + PYGLOBUS_DIR + ' exiting')
     sys.exit(-1)
 
-# Read using pyglobus
-data_file = open('data.json')
-data = json.load(data_file)
-for sht_data in data['sht_data']:
-    sht_number = sht_data[0]
-    path_to_sht_file = "../SHT/sht" + str(sht_number) + ".sht"
 
-    if not os.path.isfile(path_to_sht_file):
-        print('WARNING: unable process file\n' +
-              path_to_sht_file)
-        continue
+def get_raw_data(sht_files, signal_numbers, output_path):
 
-    sht_reader = pyglobus.util.ShtReader(path_to_sht_file)
-    fig, ax = pyplot.subplots(nrows=2, ncols=2)
-    for ind in range(0, len(SIGNAL_NUMBERS)):
-        signal_name = SIGNAL_NAMES[SIGNAL_NUMBERS[ind]]
-        # signal_name = sht_reader.get_signal_name(signal_number)
-        signal = sht_reader.get_signals(signal_name)[0]
+    for sht_file in sht_files:
+        sht_reader = pyglobus.util.ShtReader(sht_file)
+        fig, ax = pyplot.subplots(nrows=2, ncols=2)
 
-        x, y = signal.get_data_x(), signal.get_data_y()
+        for ind in range(0, len(signal_numbers)):
+            signal_name = SIGNAL_NAMES[signal_numbers[ind]]
+            signal = sht_reader.get_signals(signal_name)[0]
 
-        pyplot.subplot(2, 2, ind + 1)
+            x, y = signal.get_data_x(), signal.get_data_y()
 
-        pyplot.plot(x, y, linewidth=0.4)
-        pyplot.xlim(0.13, 0.25)
-        pyplot.xlabel('Time, s')
-        pyplot.ylabel('V')
-        pyplot.title(SENSOR_NAMES[ind])
+            pyplot.subplot(2, 2, ind + 1)
 
-    pyplot.tight_layout()
-    if not os.path.exists(OUT_DIR):
-        os.makedirs(OUT_DIR)
-    fig.savefig(OUT_DIR + 'sht' + str(sht_number) + '.png')
+            pyplot.plot(x, y, linewidth=0.4)
+            # pyplot.xlim(0.13, 0.25)
+            pyplot.xlabel('Time, s')
+            pyplot.ylabel('V')
+            pyplot.title(SENSOR_NAMES[signal_numbers[ind]])
+
+        pyplot.tight_layout()
+        fig.savefig(os.path.join(output_path, 'raw_' + os.path.splitext(os.path.basename(sht_file))[0] + '.png'))
